5740e14f314d105048a0a6e1f6c5b1e00c1b2f50
AngeloRoudani <ange.rouda@gmail.com>
2023-02-24 15:11:53 +0100

    bozza javascript

--- js/main.js
+++ js/main.js
@@ -9,25 +9,25 @@
    4 creare un (if) che esegua un operazione per cui SE i = ( i % (3*5))
    ALLORA stampi FizzBuzz al posto del multiplo di 3 o di 5.
 */
-    
-for (let i = 1 ; i <= 100 ; i++) {
+
+for (let i = 0; i <= 100; i++) {
     console.log(i);
 }
 
-if (i = ( i % 3 ) ) {
+if (i = ( i % 3 )) {
     const Fizz = "Fizz";
     let i = Fizz;
-    console.log(i)
+    console.log(i);
 
-} else if (i = ( i % 5 ) ) {
+} else if (i = ( i % 5 )) {
     const Buzz = "Buzz";
     let i = Buzz;
     console.log(i);
 
-} else if (i = ( i % (3*5))) {
+} else if (i = ( i % (3 * 5))) {
     const FizzBuzz = "FizzBuzz";
     let i = FizzBuzz;
-    console.log(i)
+    console.log(i);
 }
 
    
