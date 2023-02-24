d820e3905004f42e378b4a77d06b295e0844b717
AngeloRoudani <ange.rouda@gmail.com>
2023-02-24 16:57:09 +0100

    in progress

--- js/main.js
+++ js/main.js
@@ -9,29 +9,37 @@
    4 creare un (if) che esegua un operazione per cui SE i = ( i % (3*5))
    ALLORA stampi FizzBuzz al posto del multiplo di 3 o di 5.
 */
+
+const containerDom = document.querySelector('.container');
 let Fizz = "Fizz";
 let Buzz = "Buzz";
 let FizzBuzz = "FizzBuzz";
 
 for (let i = 1; i <= 100; i++) {
 
-    if (i % (3 * 5) == 0) {
 
-    console.log(FizzBuzz);
+    if (i % (3 * 5) == 0) {
+        let squareDom = `<div class="square"> ${FizzBuzz}</div>`;
+        containerDom.innerHTML += squareDom;
+        console.log(FizzBuzz);
 
     } else if (i % 5 == 0 ) {
-
-    console.log(Buzz);
+        let squareDom = `<div class="square"> ${Buzz}</div>`;
+        containerDom.innerHTML += squareDom;
+        console.log(Buzz);
 
     } else if (i % 3 == 0) {
-
-    console.log(Fizz);
+        let squareDom = `<div class="square"> ${Fizz}</div>`;
+        containerDom.innerHTML += squareDom;
+        console.log(Fizz);
 
     } else {
-
+        let squareDom = `<div class="square"> ${i}</div>`;
+        containerDom.innerHTML += squareDom;
         console.log(i)
 
     }
+
 
 }
 
