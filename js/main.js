f9add56fd6967c8512612e01fb0876ce91991239
AngeloRoudani <ange.rouda@gmail.com>
2023-02-24 16:03:56 +0100

    modifiche javascript

--- js/main.js
+++ js/main.js
@@ -9,25 +9,34 @@
    4 creare un (if) che esegua un operazione per cui SE i = ( i % (3*5))
    ALLORA stampi FizzBuzz al posto del multiplo di 3 o di 5.
 */
+let Fizz = "Fizz";
+let Buzz = "Buzz";
+let FizzBuzz = "FizzBuzz";
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
+
     console.log(i);
-}
 
-if (i = ( i % 3 )) {
-    const Fizz = "Fizz";
+    if (i % 3 == 0 ) {
+
     let i = Fizz;
     console.log(i);
 
-} else if (i = ( i % 5 )) {
-    const Buzz = "Buzz";
+    }  
+    if (i % 5 == 0 ) {
+
     let i = Buzz;
     console.log(i);
 
-} else if (i = ( i % (3 * 5))) {
-    const FizzBuzz = "FizzBuzz";
-    let i = FizzBuzz;
-    console.log(i);
+    } 
+    if (i % (3 * 5) == 0) {
+        let Fizz = FizzBuzz;
+        let Buzz = FizzBuzz;
+        let i = FizzBuzz;
+        console.log(i);
+
+    }
+
 }
 
-   
+
