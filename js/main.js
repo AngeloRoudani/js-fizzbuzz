5843ea55461f946e3f314c0c56503dfc556dcd50
AngeloRoudani <ange.rouda@gmail.com>
2023-02-24 14:53:59 +0100

    prima bozza javascript

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/* 1 creare un ciclo (for) che stampi i numeri da uno a 100.
+
+   2 creare un (if) che esegua un operazione per cui SE i = ( i % 3 )
+   ALLORA stampi Fizz al posto del valore normale.
+
+   3 creare un (if) che esegua un operazione per cui SE i = ( i % 5 )
+   ALLORA stampi Buzz al posto del valore normale.
+
+   4 creare un (if) che esegua un operazione per cui SE i = ( i % (3*5))
+   ALLORA stampi FizzBuzz al posto del multiplo di 3 o di 5.
+*/
+    
+for (let i = 1 ; i <= 100 ; i++) {
+    console.log(i);
+}
+
+if (i = ( i % 3 ) ) {
+    const Fizz = "Fizz";
+    let i = Fizz;
+    console.log(i)
+
+} else if (i = ( i % 5 ) ) {
+    const Buzz = "Buzz";
+    let i = Buzz;
+    console.log(i);
+
+} else if (i = ( i % (3*5))) {
+    const FizzBuzz = "FizzBuzz";
+    let i = FizzBuzz;
+    console.log(i)
+}
+
+   
