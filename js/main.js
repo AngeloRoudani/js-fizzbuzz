82161a4d840bb83ba5204a7851f4b2db2205f348
AngeloRoudani <ange.rouda@gmail.com>
2023-02-24 16:32:20 +0100

    javascript funzionante

--- js/main.js
+++ js/main.js
@@ -15,25 +15,21 @@ let FizzBuzz = "FizzBuzz";
 
 for (let i = 1; i <= 100; i++) {
 
-    console.log(i);
+    if (i % (3 * 5) == 0) {
 
-    if (i % 3 == 0 ) {
+    console.log(FizzBuzz);
 
-    let i = Fizz;
-    console.log(Fizz);
+    } else if (i % 5 == 0 ) {
 
-    }  else if (i % 5 == 0 ) {
-
-    let i = Buzz;
     console.log(Buzz);
 
-    } 
-    
-    if (i % (3 * 5) == 0) {
-        let Fizz = FizzBuzz;
-        let Buzz = FizzBuzz;
-        let i = FizzBuzz;
-        console.log(FizzBuzz);
+    } else if (i % 3 == 0) {
+
+    console.log(Fizz);
+
+    } else {
+
+        console.log(i)
 
     }
 
